7e389b8a58db38a66682e50e4fdf209835eccf5e
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-03-21 15:59:32 +0000

    First commit

--- Day1/HelloWorld.java
+++ Day1/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-
+        
+        //output
         System.out.println("Hello World!");
         System.out.println("Welcome to Java Programming!");
         System.out.println("123");
