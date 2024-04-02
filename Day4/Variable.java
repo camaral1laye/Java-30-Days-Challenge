6f20dcc0f90a8c9370c1e3b4eeee6ca16c2be174
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-04-02 01:07:13 +0000

    Your commit message here

---
+++ Day4/Variable.java
@@ -0,0 +1,22 @@
+package Day4;
+
+public class Variable {
+   
+    public static void main(String[] args){
+
+        String Name = "Laye";
+        String LastName = "Camara";
+        Name = "Laye ";
+        int Age = 20;
+        double Height = 6.0;
+        String Job = "Software Engineer";
+
+        System.out.println(Name + LastName);
+        System.out.println(Age);
+        System.out.println(Height + " ft");
+        System.out.println(Job);
+        
+    
+    }      
+    
+}
