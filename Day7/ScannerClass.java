011d36e601a9ea83a55f8554765f27b98ad20ff1
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-04-25 21:47:15 +0000

    ScannerClass Excercice

--- Day7/ScannerClass.java
+++ Day7/ScannerClass.java
@@ -7,11 +7,19 @@ public class ScannerClass{
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter Your Name:");
+        System.out.print("Enter Your String variable:");
         System.out.println(input.next());
+
+        System.out.print("Enter your boolean variable:");
         System.out.println(input.nextBoolean());
+
+        System.out.print("Enter your double variable:");
         System.out.println(input.nextDouble());
-        System.out.println(input.nextFloat());
+
+        System.out.print("Enter Your Float variable:");
+        System.out.println( input.nextFloat());
+
+        System.out.print("Enter Your integer variable:");
         System.out.println(input.nextInt());
     }
 
