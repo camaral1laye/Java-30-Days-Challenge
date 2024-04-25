2c69c99be27d24fdbc074203ec0d2869e690f236
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-04-25 03:23:12 +0000

    Your commit is here

---
+++ Day7/ExcerciseFavorite.java
@@ -0,0 +1,18 @@
+package Day7;
+
+import java.util.Scanner;
+
+public class ExcerciseFavorite {
+
+    public static void main(String[] args)
+    {
+
+    System.out.print("Enter your favorite number:");
+
+    Scanner input = new Scanner(System.in);
+    int favoriteNumber = input.nextInt();
+
+    System.out.println(favoriteNumber + " is my favorite number");
+
+    }
+}
