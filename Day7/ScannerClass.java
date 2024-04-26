b4d8a0415f8079cda8d315e2de850eee28602812
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-04-26 16:45:14 +0000

    Your commit here

--- Day7/ScannerClass.java
+++ Day7/ScannerClass.java
@@ -7,20 +7,27 @@ public class ScannerClass{
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter Your String variable:");
-        System.out.println(input.next());
 
-        System.out.print("Enter your boolean variable:");
-        System.out.println(input.nextBoolean());
+        //int
+        System.out.print("Enter Your String variable: ");
+        String s1 = input.nextLine();
+        
+        System.out.print("Enter your boolean variable: ");
+        Boolean b1 = input.nextBoolean();
+        
+        
+        
+        System.out.print("Enter Your Float variable: ");
+        float f1 = input.nextFloat();
+        
+        System.out.print("Enter Your integer variable: ");
+        int i1 = input.nextInt();
 
-        System.out.print("Enter your double variable:");
-        System.out.println(input.nextDouble());
-
-        System.out.print("Enter Your Float variable:");
-        System.out.println( input.nextFloat());
-
-        System.out.print("Enter Your integer variable:");
-        System.out.println(input.nextInt());
+        System.out.print("Enter your double variable: ");
+        double d1 = input.nextDouble();
+        System.out.println("You enter "+ (f1+i1)/d1);
+        
+        input.close();
     }
 
 }
