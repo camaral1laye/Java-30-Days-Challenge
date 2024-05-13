cd5f97cf2a4084e9630ebf8751f17bfecb33e23a
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-05-13 01:14:55 +0000

    commit

---
+++ Day9/IfElseStatement.java
@@ -0,0 +1,20 @@
+package Day9;
+
+import java.util.Scanner;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        
+        System.out.print("Enter the Number: ");
+
+        Scanner number = new Scanner(System.in);
+        int n = number.nextInt();
+
+        if(n  % 2==0)
+        System.out.println(n + " is even");
+        else
+        System.out.println( n + " is Odd");
+
+        number.close();
+    }
+}
