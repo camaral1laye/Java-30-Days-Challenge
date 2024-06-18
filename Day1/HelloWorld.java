d159564e90139927d22ccc70744e06e6291b212c
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-06-18 06:13:43 +0000

    update

--- Day1/HelloWorld.java
+++ Day1/HelloWorld.java
@@ -1,10 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         
+        String h = "Hello World!";
         //output
-        System.out.println("Hello World!");
-        System.out.println("Welcome to Java Programming!");
-        System.out.println("123");
+        System.out.println(h);
+       // System.out.println("Welcome to Java Programming!");
+       // System.out.println("123");
 
     }
 
--- Day9/PracticeExamples/LeapYear.java
+++ Day9/PracticeExamples/LeapYear.java
@@ -9,10 +9,10 @@ public class LeapYear {
         Scanner year = new Scanner(System.in);
         int y = year.nextInt();
 
-        if(y % 4 == 0 && y % 100 != 0)
+        if(y % 4 == 0 && y % 100 == 0)
         System.out.println("Leap year");
 
-        if(y % 4 != 0 || y % 100 == 0)
+        if(y % 4 != 0 || y % 100 != 0)
         System.out.println("Not a Leap year");
 
 
