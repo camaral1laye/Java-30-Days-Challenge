b44a3c9f39b72465e41c24dcd52c5b11f963424f
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-07-01 14:14:32 +0000

    Your Update is here

--- Day1/HelloWorld.java
+++ Day1/HelloWorld.java
@@ -2,9 +2,12 @@ public class HelloWorld {
     public static void main(String[] args) {
         
         String h = "Hello World!";
+        String FirstCode = "Here my first code";
+
         //output
+        
         System.out.println(h);
-       // System.out.println("Welcome to Java Programming!");
+        System.out.println(FirstCode);
        // System.out.println("123");
 
     }
