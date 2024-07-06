28d8fa2eb2a7f05d248e7b126ea9ce09d651c877
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-07-06 20:12:40 +0000

    Update

--- Day3/Excercises/Main.java
+++ Day3/Excercises/Main.java
@@ -1,8 +1,10 @@
 package Day3.Excercises;
 
 public class Main {
+  
     public static void main(String[] args) {
         
+        System.out.println("Hello world"); 
         Me.sayName();
         Me.sayAge();
 
--- Day3/Excercises/Me.java
+++ Day3/Excercises/Me.java
@@ -1,15 +1,17 @@
 package Day3.Excercises;
 
-public class Me {
+public class Me{
     public static void main(String[] args){
-
-        System.out.println("Hello World");
-
+  
+        sayName();
+        sayAge();
     }
+
     public static void sayName(){
-        System.out.println("My name is Laye");
+        System.out.println("My name is laye Camara");
     }
+
     public static void sayAge(){
-        System.out.println("I am 22 years old");
+        System.out.println("I am 24 years old");
     }
 }
