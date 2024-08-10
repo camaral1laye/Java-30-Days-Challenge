f82c047dd9f4be360ee02cdfe346e4963f6acf65
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-08-10 21:11:16 +0000

    commit here

--- Day10/NestedLoop.java
+++ Day10/NestedLoop.java
@@ -3,7 +3,7 @@ package Day10;
 public class NestedLoop {
     public static void main(String[] args) {
         for(int i =1; i <=10; i++){
-            for(int j = 1; j <= 10; j++)
+            for(int j = 1; j <=10; j++)
             System.out.print((i*j) + " ");
             System.out.println();
         }
---
+++ Extra.Excercise/NestedLoopExcercise.java
@@ -0,0 +1,17 @@
+public class NestedLoopExcercise {
+    public static void main(String[] args) {
+        
+        int rows = 8;
+
+        for(int i =1; i <= rows; i++ ){
+            for(int j = 1; j<= rows; j++){
+            System.out.print(" ");
+        }  
+        for(int k = 1; k<= i; k++){
+
+            System.out.print("*");
+        }
+            System.out.println();
+        }
+    }
+}
