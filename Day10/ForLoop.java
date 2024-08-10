bacff705e91a500d7bd4132bd8d110540156db0a
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-08-10 19:17:13 +0000

    commit here

---
+++ Day10/ForLoop.java
@@ -0,0 +1,12 @@
+package Day10;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        //int i=1;
+        for(int i = 1 ; i <=5;i++){
+            System.out.println(i);
+        }
+        //System.out.println(i); ERROR
+    }
+}
