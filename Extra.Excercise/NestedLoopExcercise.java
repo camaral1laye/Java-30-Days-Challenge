cc5da7e5dc1ea2d1159b938617f9b21f537c61a5
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-08-11 23:41:14 +0000

    commit here

--- Extra.Excercise/NestedLoopExcercise.java
+++ Extra.Excercise/NestedLoopExcercise.java
@@ -2,12 +2,12 @@ public class NestedLoopExcercise {
     public static void main(String[] args) {
         
         int rows = 8;
-
+        //int rowss = -8;
         for(int i =1; i <= rows; i++ ){
-            for(int j = 1; j<= rows; j++){
+            for(int j = 1; j<= rows - i; j++){
             System.out.print(" ");
         }  
-        for(int k = 1; k<= i; k++){
+        for(int k = 1; k<= 2 * i - 1; k++){
 
             System.out.print("*");
         }
@@ -15,3 +15,19 @@ public class NestedLoopExcercise {
         }
     }
 }
+
+        /*//botton half of the triangle
+        for(int i = rows - 1; i >= 1; i--){
+            for( int j = 1; j <= rows - i; j++){
+
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= 2 * i -1; k++){
+                System.out.print("*");
+        }
+                System.out.println();
+        }
+
+    }
+}
+        */
