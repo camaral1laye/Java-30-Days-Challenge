06c89a3af4077a0710254748a3f9405e4e7a7cfa
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-08-14 00:21:11 +0000

    commit here

---
+++ Day10/LoopExercises/LoopsExcercise1.java
@@ -0,0 +1,12 @@
+package Day10.LoopExercises;
+
+public class LoopsExcercise1 {
+    public static void main(String[] args) {
+        
+        int i = 100;
+        for(i = 1; i<=100; i++){
+            if(i %2==0)
+            System.out.println(i);
+        }
+    }
+}
