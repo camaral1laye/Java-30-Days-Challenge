73b4d505915313745c9790ad1ed66c1e8748ad0c
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-08-20 18:22:19 +0000

    commit here

--- Day10/LoopExercises/LoopsExcercise1.java
+++ Day10/LoopExercises/LoopsExcercise1.java
@@ -4,9 +4,20 @@ public class LoopsExcercise1 {
     public static void main(String[] args) {
         
         int i = 100;
+        System.out.print("Even number increasing order: ");
         for(i = 1; i<=100; i++){
             if(i %2==0)
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println(" ");
+        System.out.print("Odd number decreasing order: ");
+        for(i =100; i>= 1 ; i--){
+            if(i %2 == 0)
+            System.out.print(i + " ");
+        }
+       
     }
+       
 }
+
+    
